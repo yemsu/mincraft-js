26fe108ed48be650cac73f143a1bff910ad47260
정수진 <sssjsjj@gmail.com>
2024-03-15 16:52:12 +0900

    feat: draw call을 줄여 성능개선을 위해 instanced Mesh 적용

--- scripts/world.js
+++ scripts/world.js
@@ -5,18 +5,27 @@ const material = new THREE.MeshStandardMaterial({ color: 0x00d000 })
 
 // group은 씬에 들어가는 object collection 같은 것
 export class World extends THREE.Group {
-	constructor(size = 32) {
+	constructor(size = { width: 64, height: 32 }) {
 		super()
 		this.size = size
 	}
 
 	generate() {
-		for (let x = 0; x < this.size; x++) {
-			for (let z = 0; z < this.size; z++) {
-				const block = new THREE.Mesh(geometry, material)
-				block.position.set(x, 0, z)
-				this.add(block)
+		const maxCount = this.size.width * this.size.width * this.size.height
+		const mesh = new THREE.InstancedMesh(geometry, material, maxCount)
+		mesh.count = 0 // current number of instances that we have
+
+		const matrix = new THREE.Matrix4() // store the position of each block
+		for (let x = 0; x < this.size.width; x++) {
+			for (let y = 0; y < this.size.height; y++) {
+				for (let z = 0; z < this.size.width; z++) {
+					// 0.5 더하는 이유: 블럭의 중심이 0,0,0이기 때문에 중심을 맞추기 위해 0.5를 더함
+					matrix.setPosition(x + 0.5, y + 0.5, z + 0.5)
+					mesh.setMatrixAt(mesh.count++, matrix)
+				}
 			}
 		}
+
+		this.add(mesh)
 	}
 }
