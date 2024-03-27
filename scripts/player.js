d5c69f7fbc3f049e46e85f587592df688e0d626e
정수진 <sssjsjj@gmail.com>
2024-03-27 10:14:54 +0900

    feat: player 위치 텍스트 추가

--- scripts/player.js
+++ scripts/player.js
@@ -27,6 +27,8 @@ export class Player {
 			this.velocity.z = this.input.z
 			this.controls.moveRight(this.velocity.x * dt)
 			this.controls.moveForward(this.velocity.z * dt)
+
+			document.getElementById('player-position').innerHTML = this.toString()
 		}
 	}
 
@@ -74,5 +76,17 @@ export class Player {
 				this.input.x = 0
 				break
 		}
+	}
+
+	/**
+	 * Returns player position in a readable string form
+	 * @returns {string}
+	 */
+	toString() {
+		let str = ''
+		str += `X: ${this.position.x.toFixed(3)}`
+		str += `Y: ${this.position.x.toFixed(3)}`
+		str += `Z: ${this.position.x.toFixed(3)}`
+		return str
 	}
 }
